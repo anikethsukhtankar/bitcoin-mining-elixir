a3841fcb80daaff13576b442e39d7e81919dc1ef
Shikha Mehta <shikha.mehta@ufl.edu>
2017-09-18 00:21:53 -0400

    Changed distribution logic to support multiple clients at a time

--- lib/Worker.ex
+++ lib/Worker.ex
@@ -5,6 +5,22 @@ defmodule BitcoinMining.Worker do
         make_distributed(Enum.reverse(iflist),length(iflist))
         
         Node.connect(String.to_atom("server@" <> hd(parameter)))
+        client_id = spawn(fn -> talk_to_server() end)
+        :global.sync()
+        send(:global.whereis_name(:serve),{:getk,client_id})
+        receive do: (_ -> :ok)
+    end
+
+    def talk_to_server() do
+        {:ok, pid} = Task.Supervisor.start_link()
+        logproc = System.schedulers_online()
+        receive do
+            {:sendk,k_value} -> k = k_value
+        end
+        :global.sync()
+        for _ <- 1..logproc do
+            Task.Supervisor.async(pid,fn() -> miner(:global.whereis_name(:serve),k) end)
+        end
         receive do: (_ -> :ok)
     end
 
@@ -20,7 +36,7 @@ defmodule BitcoinMining.Worker do
                         IO.puts "Could not make current node distributed."
                     end
                 else
-                    server_node_name = String.to_atom("worker@" <> current_ip)
+                    server_node_name = String.to_atom(randomizer(5) <> "_wkr@" <> current_ip)
                     Node.start(server_node_name)
                     Node.set_cookie(server_node_name,:monster)
                 end
@@ -28,6 +44,19 @@ defmodule BitcoinMining.Worker do
                 _ -> if l > 1, do: make_distributed(tail,l-1), else: IO.puts "Could not make current node distributed."
             end
         end
+    end
+
+    def miner(s_id,k) do
+      msgt = "asukhtankar" <> randomizer(9)
+      temp = :crypto.hash(:sha256, msgt) |> Base.encode16 |> String.downcase
+      if(String.slice(temp,0,k) === String.duplicate("0",k)) do
+        send(s_id, msgt <> "\t" <> temp)
+      end
+      miner(s_id,k)
+    end
+    
+    def randomizer(l) do
+      :crypto.strong_rand_bytes(l) |> Base.url_encode64 |> binary_part(0, l) |> String.downcase
     end
 
   end
