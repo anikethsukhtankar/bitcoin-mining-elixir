0e467ed2e94e5c092c61cb2aa4e6a7eccc8d0da3
Shikha Mehta <shikha.mehta@ufl.edu>
2017-09-17 22:04:05 -0400

    Added logic to check every IP in the retrieved list

--- lib/Worker.ex
+++ lib/Worker.ex
@@ -1,26 +1,31 @@
 defmodule BitcoinMining.Worker do
  
     def process(parameter) do
-        try do
-            unless Node.alive?() do
-                {:ok,iflist}=:inet.getif()
-                [head | tail] = Enum.reverse(iflist)
+        {:ok,iflist}=:inet.getif()
+        make_distributed(Enum.reverse(iflist),length(iflist))
+        
+        Node.connect(String.to_atom("server@" <> hd(parameter)))
+        receive do: (_ -> :ok)
+    end
+
+    def make_distributed([head | tail],l) do
+        unless Node.alive?() do
+            try do
                 {ip_tuple,_,_} = head
                 current_ip = to_string(:inet_parse.ntoa(ip_tuple))
                 if current_ip === "127.0.0.1" do
-                    {ip_tuple,_,_} = hd(tail)
-                    current_ip = to_string(:inet_parse.ntoa(ip_tuple))
+                    if l != 1 do
+                        make_distributed(tail,l-1)
+                    end
+                else
+                    server_node_name = String.to_atom("worker@" <> current_ip)
+                    Node.start(server_node_name)
+                    Node.set_cookie(server_node_name,:monster)
                 end
-                current_ip = "192.168.0.2"
-                worker_node_name = String.to_atom("worker@" <> current_ip)
-                Node.start(worker_node_name)
-                Node.set_cookie(worker_node_name,:monster)
+            rescue
+                _ -> if l != 1, do: make_distributed(tail,l-1), else: IO.puts "Could not make current node distributed."
             end
-        rescue
-	        _ -> IO.puts "An error occurred while making worker node distributed."
         end
-        Node.connect(String.to_atom("server@" <> hd(parameter)))
-        receive do: (_ -> :ok)
     end
 
   end
